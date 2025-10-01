50108dda1f9e3b8a4ee8eb8268781619a71828c9
Ishan <ishan@Ishans-MacBook-Air.local>
2025-10-01 15:07:44 +0530

    Parenthesis

--- src/Stacks_and_Queues/Balanced.java
+++ src/Stacks_and_Queues/Balanced.java
@@ -1,4 +1,28 @@
 package Stacks_and_Queues;
 
 public class Balanced {
+    public static void main(String[] args) {
+      Balanced b = new Balanced();
+      boolean result = b.checkValidString("(*)))");
+      System.out.println(result);
+    }
+
+    public boolean checkValidString(String s) {
+        int c1 = 0, c2 =0;
+        for(int i = 0 ; i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='('){
+                c1++;
+                c2++;
+            }else if(c==')'){
+                c1--;
+                c2 = Math.max(c2-1,0);
+            }else{
+                c1++;
+                c2 = Math.max(c2-1,0);
+            }
+            if(c1<0) return false;
+        }
+        return c2==0;
+    }
 }
