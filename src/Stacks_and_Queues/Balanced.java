137ac45ce4522896c2ead53f2989e831c92d844a
Ishan <ishan@Ishans-MacBook-Air.local>
2025-10-01 15:12:23 +0530

    Comment

--- src/Stacks_and_Queues/Balanced.java
+++ src/Stacks_and_Queues/Balanced.java
@@ -6,11 +6,12 @@ public class Balanced {
       boolean result = b.checkValidString("(*)))");
       System.out.println(result);
     }
-
+ //Problem Valid Parenthesis
     public boolean checkValidString(String s) {
         int c1 = 0, c2 =0;
         for(int i = 0 ; i<s.length();i++){
             char c = s.charAt(i);
+
             if(c=='('){
                 c1++;
                 c2++;
