08c2487589ec52f0267862d2b8ecff7df5f5996a
Ishan <ishan@Ishans-MacBook-Air.local>
2025-10-04 11:36:43 +0530

    K_Largest_Element

---
+++ src/Heaps/K_Larget_Element.java
@@ -0,0 +1,45 @@
+package Heaps;
+import java.util.PriorityQueue;
+
+//Given an integer array nums and an integer k, return the kth largest element in the array.
+//
+//Note that it is the kth largest element in the sorted order, not the kth distinct element.
+//
+//Can you solve it without sorting?
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [3,2,1,5,6,4], k = 2
+//Output: 5
+//Example 2:
+//
+//Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+//Output: 4
+
+
+public class K_Larget_Element {
+    public static void main(String[] args) {
+        K_Larget_Element kle = new K_Larget_Element();
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        int result = kle.findKthLargest(nums,k);
+        System.out.println(result);
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0 ; i<k ; i++){
+            pq.add(nums[i]);
+        }
+        for(int i = k ; i<nums.length ; i++){
+            if(nums[i]>pq.peek()){
+                pq.remove();
+                pq.add(nums[i]);
+            }
+        }
+        return pq.peek();
+
+    }
+}
